01318f21fafe8bf1cad050abe0514378d158d38e
mesepith <zahiralam29@gmail.com>
2024-03-15 01:36:02 +0530

    set dynamic screen width and height

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -1,13 +1,15 @@
 // utils/Constants.ts
 
 // Example screen dimensions, adjust according to your game's design
-export const SCREEN_WIDTH: number = 800;
-export const SCREEN_HEIGHT: number = 600;
+import { Dimensions } from 'react-native';
+
+export const SCREEN_WIDTH = Dimensions.get('window').width;
+export const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 // Car settings, adjust as needed
 // Adjust the car's start position to be at the bottom center of the screen
 export const CAR_START_POSITION: {x: number, y: number} = {
-    x: SCREEN_WIDTH / 2 - 250, // Assuming the car's width is 50, adjust as needed
+    x: SCREEN_WIDTH / 2 - 25, // Assuming the car's width is 50, adjust as needed
     y: SCREEN_HEIGHT - 100, // Assuming the car's height is 100, adjust as needed
   };
 // Set the car's velocity to move upwards. Adjust the value as needed for speed.
